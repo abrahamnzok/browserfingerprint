6814f78faca76010a4f216742277faf9b49bc6eb
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-10 15:40:43 +0100

    server configuration file

---
+++ src/server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const fingerRoute = require('./routes/fingerRoute');
+const port = process.env.PORT || 3300;
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/fingerprint',fingerRoute);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
