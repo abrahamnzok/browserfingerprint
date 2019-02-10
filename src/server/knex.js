6729dbdcb21d3a309a047b72e4ed09c74581ce20
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-10 17:23:27 +0100

    using env variables

--- src/server/knex.js
+++ src/server/knex.js
@@ -1,10 +1,10 @@
 module.exports = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        port: 5432,
-        database: 'fingerprint'
+        host: process.env.pg_host,
+        user: process.env.pg_user,
+        password: process.env.pg_pwd,
+        port: process.env.pg_port,
+        database: process.env.pg_db
     }
 });
