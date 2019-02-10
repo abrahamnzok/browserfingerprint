29b688a39eccc58b27d05295d38d76f74c9b9fca
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-10 17:15:43 +0100

    removed exist route handler function

--- src/server/routes/fingerRoute.js
+++ src/server/routes/fingerRoute.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {createHandler} = require('../handlers/fingerprintHandler');
+const {createHandler, doesExist} = require('../handlers/fingerprintHandler');
 
 create = async (req, res) => {
     const {fingerprint} = req.body;
-    createHandler(fingerprint);
-    return res.status(200);
+    const fingerPrintExists = await doesExist(fingerprint);
+    if(!fingerPrintExists) {
+        createHandler(fingerprint);
+        return res.sendStatus(200);
+    }else{
+        return res.send({
+            exist : fingerPrintExists,
+            status: 409,
+        })
+    }
 };
 
-
-
 router.post('/create', create);
+
 module.exports = router;
