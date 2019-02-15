62587e587b99da8a267063d2431b41aac4742241
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-15 17:36:40 +0100

    new comparison utils module

---
+++ src/server/utils/utils.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+
+isFingerHashPrintUnique = (result, fingerprint) => {
+    const data  = _.head(result);
+    const dbfingerprint = data === undefined ? false : data.key;
+    return dbfingerprint === false ? false : _.isEqual(dbfingerprint, fingerprint);
+};
+
+renderFingerprint = (result) => {
+    const data = _.head(result);
+    return data === undefined ? false : data.fingerprint;
+};
+
+renderKey = (result) => {
+    const data = _.head(result);
+    return data === undefined ? false : data.key;
+};
+
+areFingerPrintsEqual = (fingerprintA, fingerprintB) => {
+    return _.isEqual(fingerprintA, fingerprintB);
+};
+
+module.exports = {
+    isFingerHashPrintUnique : isFingerHashPrintUnique,
+    areFingerPrintsEqual: areFingerPrintsEqual,
+    renderFingerprint : renderFingerprint
+};
