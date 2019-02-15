6b51e8d08741e6ff52f0236803cfee31a41475e6
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-15 16:59:59 +0100

    knex new config variables

--- src/server/knex.js
+++ src/server/knex.js
@@ -2,7 +2,7 @@ module.exports = require('knex')({
     client: 'pg',
     connection: {
         port: process.env.PG_PORT,
-        database: process.env.PG_DB,
+        database: process.env.PG_DATABASE,
         user : process.env.PG_USER,
         password : process.env.PG_PWD,
     },
