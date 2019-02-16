b3f971161baf37147cb7031143c5bb76e704b339
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-16 15:49:13 +0100

    moved this logic inside Dataviz component

--- src/components/Dataviz.vue
+++ src/components/Dataviz.vue
@@ -6,9 +6,9 @@
 </template>
 
 <script>
-    import Chart from 'chart.js';
-    import {getDatasets, getOptions} from '../server/utils/dataviz';
-    import * as axios from "axios";
+    import Chart from 'chart.js'
+    import * as axios from "axios"
+    import randomColor from 'randomcolor'
 
     export default {
         name: "Dataviz",
@@ -19,7 +19,7 @@
             }
         },
         methods: {
-            createBar: (id, datasets, options) => {
+            createBar (id, datasets, options) {
                 const properties = {
                     type: 'bar',
                     data: datasets,
@@ -28,6 +28,64 @@
                 const ctx = document.getElementById(id).getContext('2d');
                 new Chart(ctx, properties);
             },
+
+            getLabels(results) {
+                return results.map(o => o.name);
+            },
+
+            getVisits (results) {
+                return results.map(o => o.visits);
+            },
+
+            getCollisions (results) {
+                return results.map(o => o.collisions);
+            },
+
+            getColors (results) {
+                return randomColor({
+                    count: results.length,
+                })
+            },
+
+            getDatasets (results) {
+                return {
+                    labels: this.getLabels(results),
+                    datasets: [
+                        {
+                            label: 'Number of collisions',
+                            backgroundColor: this.getColors(results),
+                            data: this.getCollisions(results),
+                            borderWidth: 1
+                        },
+                        {
+                            label: 'Number of visits',
+                            backgroundColor: this.getColors(results),
+                            data: this.getVisits(results),
+                            borderWidth: 1
+                        },
+                    ]
+                }
+            },
+
+            getOptions () {
+                return {
+                    responsive: true,
+                    scales: {
+                        xAxes: [{
+                            ticks: {
+                                maxRotation: 90,
+                                minRotation: 80
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            },
+
         },
         async created() {
             try {
@@ -35,8 +93,8 @@
                     method: 'get',
                     url: 'http://localhost:3300/viz'
                 });
-                this.datasets = getDatasets(results.data);
-                this.options = getOptions();
+                this.datasets = this.getDatasets(results.data);
+                this.options = this.getOptions();
                 this.createBar('dataviz', this.datasets, this.options);
             } catch (e) {
                 console.log(e.stack);
