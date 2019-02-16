5effd3354c103171241a1b72e83dadf977563873
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-16 13:57:41 +0100

    app is using the dataviz router to handle request on the viz route.

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const fingerRoute = require('./routes/fingerRoute');
+const datavizRoute = require('./routes/datavizRoute');
 const port = process.env.PORT || 3300;
 
 app.use(cors({
@@ -13,5 +14,6 @@ app.use(cors({
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/fingerprint', fingerRoute);
+app.use('/viz', datavizRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
